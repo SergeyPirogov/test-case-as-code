6023793a3499c1fdc1e077e85f7cec42d0338b3e
Serhii Pirohov <sergio_89@ukr.net>
2019-05-27 12:45:14 +0300

    link annotation for case id added

--- common/src/main/java/com/qaguild/annotations/trail/CaseId.java
+++ common/src/main/java/com/qaguild/annotations/trail/CaseId.java
@@ -9,7 +9,7 @@ import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
-@LinkAnnotation
+@LinkAnnotation(type = "tms")
 public @interface CaseId {
     int value();
 }
